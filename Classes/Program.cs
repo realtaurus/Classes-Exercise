e08ea51b80529cee47a243eb231fded45db15cb7
TaurusA <realtaurus97@gmail.com>
2024-02-04 11:21:56 -0600

    My class Addition

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Classes
+﻿public class Car
 {
-    public class Program
+    // Properties
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
+class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
+        Car taurusCar = new Car();
+        taurusCar.Make = "Jeep";
+        taurusCar.Model = "Grand Cherokee";
+        taurusCar.Year = 2021;
+        Console.WriteLine($"{taurusCar.Make} is my favorite suv.");
+        Console.WriteLine($"{taurusCar.Model} is the best version.");
+        Console.WriteLine($"{taurusCar.Year} is the last production year");
     }
 }
+
